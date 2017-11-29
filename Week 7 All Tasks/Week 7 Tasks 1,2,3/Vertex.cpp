5312a1a5c4be02903732ba32bfcfe91e2d97701b
Alex <aduxbury0@hotmail.co.uk>
2017-11-29 01:39:20 +0000

    cleaned up week 7

--- Week 7 All Tasks/Week 7 Tasks 1,2,3/Vertex.cpp
+++ Week 7 All Tasks/Week 7 Tasks 1,2,3/Vertex.cpp
@@ -18,27 +18,3 @@ Vertex::~Vertex()
 	std::cout << "Node of the value: " << vertexValue << " has been deleted" << std::endl;
 
 }
-
-/*void Vertex::createEdge(Vertex* inputVertex, Vertex* edgeTo) {
-
-	inputVertex->vectorOfEdges.push_back(edgeTo);
-
-
-}
-
-void Vertex::deleteEdge(Vertex* parentVertex, Vertex* vertexToDelete) {
-
-	for (int i = 0; i < parentVertex->vectorOfEdges.size(); i++) {
-
-		if (parentVertex->vectorOfEdges[i] == vertexToDelete) {
-
-			parentVertex->vectorOfEdges.erase(parentVertex->vectorOfEdges.begin() + i);
-
-		}
-
-
-
-	}
-}
-
-*/
